578b3bba6402a3ef41cb85c7b0815a3000a308be
peterrexj <peterrexj@hotmail.com>
2023-10-11 21:34:31 +1100

    1.0.1.8

--- buildpack/PackProjects.ps1
+++ buildpack/PackProjects.ps1
@@ -1,6 +1,7 @@
 ﻿
 Set-ExecutionPolicy Bypass
 
+$pjOutput = [IO.Path]::Combine($PSScriptRoot, '..\Output\')
 $jira = [IO.Path]::Combine($PSScriptRoot, '..\Jira.Rest.Sdk\Jira.Rest.Sdk.csproj')
 
-dotnet pack $jira
+dotnet pack $jira --output $pjOutput
--- buildpack/PublishProjects.ps1
+++ buildpack/PublishProjects.ps1
@@ -1,9 +1,9 @@
 ﻿Set-ExecutionPolicy Bypass
 
 $apiKey = [System.Environment]::GetEnvironmentVariable('NugetApiKey', 'User')
-$packageVersion = '.1.0.1.8.nupkg'
+$packageVersion = '.1.0.1.9.nupkg'
 
-$jira = [IO.Path]::Combine($PSScriptRoot, '..\Jira.Rest.Sdk\bin\Debug\Jira.Rest.Sdk' + $packageVersion)
+$jira = [IO.Path]::Combine($PSScriptRoot, '..\Output\Jira.Rest.Sdk' + $packageVersion)
 
 Get-ChildItem -Path $jira -ErrorAction Stop
 
