a075be7ab54b6686110c50e81270ce8c90575b3a
peterrexj <peterrexj@hotmail.com>
2025-11-04 01:01:45 +1100

    1.0.5

--- buildpack/PublishProjects.ps1
+++ buildpack/PublishProjects.ps1
@@ -1,7 +1,7 @@
 ﻿Set-ExecutionPolicy Bypass
 
 $apiKey = [System.Environment]::GetEnvironmentVariable('NugetApiKey', 'User')
-$packageVersion = '.1.0.5.nupkg'
+$packageVersion = '.1.0.6.nupkg'
 
 $jira = [IO.Path]::Combine($PSScriptRoot, '..\Output\Jira.Rest.Sdk' + $packageVersion)
 
